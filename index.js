8046f3efaa03bb7f38ae050bd5cd2f8220d9086b
js8g18 <js8g18@soton.ac.uk>
2022-07-20 19:24:54 +0100

    add style and js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { createApp } = Vue;
 
-createApp({
-  data() {
-    return {
-      message: "Hello Vue!",
-    };
-  },
-}).mount("#app");
+const app = {
+    data() {
+        return {
+          message: "Hello Vue!",
+        };
+      },
+}
+
+createApp(app).mount("#app");
