1880c88d80768f74bf24f3dd318eaf3f9f54c637
js8g18 <js8g18@soton.ac.uk>
2022-07-26 14:49:41 +0100

    add: nav bar state

--- index.js
+++ index.js
@@ -1,53 +1,82 @@
 const { createApp } = Vue;
 
-const app = {
-    data() {
-        return {
-          message: "Hello Vue!",
-          page: "about",
-          selected1: "white",
-          selected2: "white",
-          selected3: "white",
-        };
-      },
-    methods: {
-        switchPage(destination){
-            if (destination==="about"){
-                this.selected1="rgba(126, 126, 126, 0.701)"
-            } else if (destination==="projects"){
-                this.selected2="rgba(126, 126, 126, 0.701)"
-            } else if (destination==="contact"){
-                this.selected3="rgba(126, 126, 126, 0.701)"
-            } else if (destination==="left"){
-                switch(this.page){
-                    case "about":
-                        return;
-                    case "projects":
-                        this.page = "about";
-                        this.selected1="rgba(126, 126, 126, 0.701)"
-                        return;
-                    case "contact":
-                        this.page = "projects"
-                        this.selected2="rgba(126, 126, 126, 0.701)"
-                        return;
-                }
-            } else if (destination==="right"){
-                switch(this.page){
-                    case "about":
-                        this.page = "projects"
-                        this.selected2="rgba(126, 126, 126, 0.701)"
-                        return;
-                    case "projects":
-                        this.page = "contact";
-                        this.selected1="rgba(126, 126, 126, 0.701)"
-                        return;
-                    case "contact":
-                        return;
-                }
-            }
-            this.page = destination;
-        }
+class Button {
+  constructor(number) {
+    if (number === 1) {
+      this.active = true;
+      this.inactive = false;
+    } else {
+      this.active = false;
+      this.inactive = true;
     }
+  }
+
+  On() {
+    this.active = true;
+    this.inactive = false;
+  }
+
+  Off() {
+    this.active = false;
+    this.inactive = true;
+  }
 }
+
+const app = {
+  data() {
+    return {
+      message: "Hello Vue!",
+      page: "about",
+      buttonStates: [new Button(1), new Button(2), new Button(3)],
+    };
+  },
+  methods: {
+    switchButtons(index) {
+      for (const i in this.buttonStates) {
+        if (i == index) {
+          this.buttonStates[i].On();
+        } else {
+          this.buttonStates[i].Off();
+        }
+      }
+    },
+    switchPage(destination) {
+      if (destination === "about") {
+        this.switchButtons(0);
+      } else if (destination === "projects") {
+        this.switchButtons(1);
+      } else if (destination === "contact") {
+        this.switchButtons(2);
+      } else if (destination === "left") {
+        switch (this.page) {
+          case "about":
+            return;
+          case "projects":
+            this.page = "about";
+            this.switchButtons(0);
+            return;
+          case "contact":
+            this.page = "projects";
+            this.switchButtons(1);
+            return;
+        }
+      } else if (destination === "right") {
+        switch (this.page) {
+          case "about":
+            this.page = "projects";
+            this.switchButtons(1);
+            return;
+          case "projects":
+            this.page = "contact";
+            this.switchButtons(2);
+            return;
+          case "contact":
+            return;
+        }
+      }
+      this.page = destination;
+    },
+  },
+};
 
 createApp(app).mount("#app");
